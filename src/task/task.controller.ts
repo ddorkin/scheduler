8a8fbbb086326d5d6f53f8fabb6ce07f5a5c2aa6
dmitriy <dmitriydorkin@gmail.com>
2022-07-13 14:57:14 +0300

    add redis config in docker compose file

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { TaskService } from './task.service';
 
+// todo: нужно дописать остальные методы для работы с кронами (добавление, изменение, удаление)
 @Controller('task')
 export class TaskController {
   constructor(private serv: TaskService) {}
